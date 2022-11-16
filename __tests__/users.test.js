d971b6c0c2c1609f4c530f56651e02cbb0deabe6
Rio Edwards <rioredwards@gmail.com>
2022-11-16 14:12:56 -0800

    Add authenticate middleware

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService.js');
 
 describe('users', () => {
   beforeEach(() => {
@@ -33,6 +34,23 @@ describe('users', () => {
     const res = await request(app)
       .post('/api/v1/users/sessions')
       .send({ email: 'test@example.com', password: '12345' });
+    expect(res.status).toEqual(200);
+  });
+
+  it('GET api/v1/users/protected should return a 401 if not authenticated', async () => {
+    const res = await request(app).get('/api/v1/users/protected');
+    expect(res.status).toEqual(401);
+  });
+
+  it('api/v1/users/protected should return the current user if authenticated', async () => {
+    const agent = request.agent(app);
+    // create user
+    await UserService.create({ ...mockUser });
+    // sign in user
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@example.com', password: '12345' });
+    const res = await agent.get('/api/v1/users/protected');
     expect(res.status).toEqual(200);
   });
 
