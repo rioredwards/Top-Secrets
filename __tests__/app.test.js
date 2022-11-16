077f4c910b03442d54f3d59950a4655ec8f12dcd
Rio Edwards <rioredwards@gmail.com>
2022-11-15 18:03:37 -0800

    Add Create User route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,33 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('users', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  // Dummy user for testing
+  const mockUser = {
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'test@example.com',
+    password: '12345',
+  };
+
+  it('POST /api/v1/users creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    expect(res.status).toBe(200);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
